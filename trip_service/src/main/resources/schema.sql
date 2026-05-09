4fde06f0482a56ca268379f3a48623834441481d
hikaruvi <daniilrybkin32@gmail.com>
2026-05-09 17:53:48 +0700

    feat: add migrations

--- trip_service/src/main/resources/schema.sql
+++
@@ -1,1 +0,0 @@
-CREATE INDEX IF NOT EXISTS idx_trips_status ON trips(status);
--- worker_service/src/main/resources/schema.sql
+++
@@ -1,2 +0,0 @@
-CREATE INDEX IF NOT EXISTS idx_notification_tasks_status ON notification_tasks(status);
-CREATE INDEX IF NOT EXISTS idx_notification_tasks_trip_id ON notification_tasks(trip_id);
